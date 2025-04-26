538e6e6931dcd5a5278b038f57d7175bb8f8e480
Trupti Bhatkar <truptibhatkar2002@gmail.com>
2025-04-26 19:55:04 +0530

    added file

---
+++ prompt_ui.py
@@ -0,0 +1,25 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import streamlit as st 
+
+load_dotenv()
+
+llm=HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    task="text-generation"
+)
+
+model=ChatHuggingFace(llm=llm)
+
+
+
+st.header("Ask me anything..")
+
+user_input=st.text_input("enter your prompt..")
+
+
+if st.button("submit"):
+    res=model.invoke(user_input)
+    st.write(res.content)
+
+
